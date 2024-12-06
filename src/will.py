7dbd26b03d4f93626cc6040ee02da684c6c49b9a
gswirrl <guyjohnhilton@gmail.com>
2024-12-06 03:01:42 +0000

    refactor

--- src/will.py
+++ src/will.py
@@ -1,6 +1,6 @@
 from pydantic import BaseModel
 from openai import OpenAI
-from functools import reduce
+from text import get_text
 import os
 import json
 
@@ -27,38 +27,6 @@ def get_adjectives(text):
     )
     return(completion.choices[0].message.content)
     
-
-def strip_whitespace(str):
-    return " ".join(str.split())
-
-
-def replace(str, replacements):
-    return reduce(lambda a, kv: a.replace(*kv), replacements, str)
-
-
-def clean(str):
-    replacements = [("[new page]", "")]
-    return replace(strip_whitespace(str), replacements)
-
-
-def load_text(filename):
-    with open(filename, "r", encoding="utf-8") as file:
-        data = file.read()
-    return data
-
-
-def snippet(str, left, right):
-    start = str.find(left)
-    end = str.find(right) + len(right)
-    return str[start:end]
-
-
-def include(target_str, passages):
-    return " ".join(map(lambda tup: snippet(target_str, tup[0], tup[1]), passages))
-
-
-def get_text(filename, including):
-    return (include(clean(load_text(filename)), including))
 
 def process(filename, including):
     text = get_text(filename, including)
